af2a11ab4bac552c1062d57e899c5c64d97d8526
Johannes Wagner <jwagner@audeering.com>
2022-06-09 12:36:48 +0200

    TST: update test_load.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,13 @@
 import glob
 import os
-import random
 import shutil
 
-import numpy as np
-import opensmile
-
 import pytest
-import torch
 
 import audeer
 import audiofile
 import audobject
+import opensmile
 
 
 pytest.ROOT = audeer.safe_path(
@@ -38,44 +34,6 @@ pytest.FEATURE_SHAPE = pytest.FEATURE(
     pytest.SIGNAL,
     pytest.SAMPLING_RATE,
 ).shape[1:]
-
-# fix seed
-
-seed = 1234
-torch.backends.cudnn.deterministic = True
-torch.manual_seed(seed)
-torch.cuda.manual_seed_all(seed)
-np.random.seed(seed)
-random.seed(seed)
-
-
-# create model with single output node
-
-class TorchModel(torch.nn.Module):
-
-    def __init__(
-        self,
-    ):
-        super().__init__()
-        self.hidden = torch.nn.Linear(pytest.FEATURE_SHAPE[0], 8)
-        self.out = torch.nn.Linear(8, 2)
-
-    def forward(self, x: torch.Tensor):
-        y = self.hidden(x.mean(dim=-1))
-        y = self.out(y)
-        return y.squeeze()
-
-
-pytest.MODEL_PATH = os.path.join(pytest.TMP, 'model.onnx')
-torch.onnx.export(
-    TorchModel(),
-    torch.randn(pytest.FEATURE_SHAPE),
-    pytest.MODEL_PATH,
-    input_names=['feature'],
-    output_names=['gender'],
-    dynamic_axes={'feature': {1: 'time'}},
-    opset_version=12,
-)
 
 
 # clean up
