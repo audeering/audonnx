4b5e452851eb1f33107e8e8e6d3d892de622c2f5
Hagen Wierstorf <hwierstorf@audeering.com>
2026-04-15 10:28:24 +0200

    Fix linter

--- audonnx/core/testing.py
+++ audonnx/core/testing.py
@@ -1,12 +1,12 @@
 from collections.abc import Sequence
 
 import numpy as np  # noqa: F401, needed for doctest
+import onnx
 import onnxruntime
 
 from audonnx.core.function import Function
 from audonnx.core.model import Model
 from audonnx.core.typing import Device
-import onnx
 
 
 def create_model(
